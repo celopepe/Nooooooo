f1f32377aaf9f38e54d1069085641eed09bebf66
celopepe <132950040+celopepe@users.noreply.github.com>
2023-05-09 00:12:29 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
         <img src="/b2753328-1f88-4e64-ac9e-4c7f85a79bc4.jpg" class="vader" />
         <iframe width="110" height="200" src="https://www.myinstants.com/instant/darth-vader-noooooooo-94359/embed/" frameborder="0" scrolling="no"></iframe>
         <p className="description">

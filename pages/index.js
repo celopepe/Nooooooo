d5b879fce59d8beac9e5ff03431e0f73505d10ef
celopepe <132950040+celopepe@users.noreply.github.com>
2023-05-09 00:21:34 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <img src="/b2753328-1f88-4e64-ac9e-4c7f85a79bc4.jpg" style="margin: 20px 20px 20px 20px class="vader" />
+        <img src="/b2753328-1f88-4e64-ac9e-4c7f85a79bc4.jpg" style="margin: 20px 20px 20px 20px" class="vader" />
         <iframe width="110" height="200" src="https://www.myinstants.com/instant/darth-vader-noooooooo-94359/embed/" frameborder="0" scrolling="no"></iframe>
         <p className="description">
           Get started by editing <code>pages/index.js</code>

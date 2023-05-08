d108f0850aff4114341bab4d50bfb9b3fcce33c2
celopepe <132950040+celopepe@users.noreply.github.com>
2023-05-09 01:09:50 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,6 @@ export default function Home() {
       <main>
         <img src="/b2753328-1f88-4e64-ac9e-4c7f85a79bc4.jpg" class="vader" />
         <iframe width="110" height="100" src="https://www.myinstants.com/instant/darth-vader-noooooooo-94359/embed/" frameborder="0" scrolling="no"></iframe>
-        <p className="description">
-          <code>Contract: </code>
-        </p>
-      </main>
     
     <Footer />
 
